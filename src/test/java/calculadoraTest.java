b6fc13283348f2b0935825db7982b1168f8c54bd
sebastianR11 <sebastian.ruiz@alumnos.uv.cl>
2021-06-10 01:27:27 -0400

    Floats bien formateados en testing

--- src/test/java/calculadoraTest.java
+++ src/test/java/calculadoraTest.java
@@ -11,22 +11,22 @@ class calculadoraTest {
 
     @Test
     void adicion() {
-        assertEquals(5.1,calculadora.adicion(3.1,2));
+        assertEquals(5.1f,calculadora.adicion(3.1f,2));
     }
 
     @Test
     void substraccion() {
-        assertEquals(1.1,calculadora.substraccion(3.1,2));
+        assertEquals(1.1f,calculadora.substraccion(3.1f,2));
     }
 
     @Test
     void multiplicacion() {
-        assertEquals(6.2,calculadora.multiplicacion(3.1,2));
+        assertEquals(6.2f,calculadora.multiplicacion(3.1f,2));
     }
 
     @Test
     void division() {
-        assertEquals(1.55,calculadora.division(3.1,2));
+        assertEquals(1.55f,calculadora.division(3.1f,2));
     }
 
 
